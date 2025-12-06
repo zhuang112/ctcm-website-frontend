0df778e45c94de7e8f4291fac859752655bf67ac
zhuang112 <zhuang.usa112@gmail.com>
2025-12-06 21:30:56 +0800

    auto update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ctcm-website-frontend/', // 👈 填「你的 repo 名稱」，前後都要有斜線
 })
